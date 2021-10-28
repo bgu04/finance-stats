24b2c82d05b9876bbf1f92cbf8eb2765c91b37c1
richard.gu <richard.gu@navihealth.com>
2021-10-28 13:18:45 -0400

    added performance data

--- server/finance-data-server.py
+++ server/finance-data-server.py
@@ -6,24 +6,68 @@ from flask_cors import CORS
 from dateutil import parser
 from bson.objectid import ObjectId
 import time
+from datetime import datetime
+from datetime import date
+import requests
+
 
 app = Flask(__name__)
 
 app.config['MONGO_DBNAME'] = 'finance'
-app.config['MONGO_URI'] = 'mongodb://localhost:27017/finance'
+app.config['MONGO_URI'] = 'mongodb://192.168.1.155:27017/finance'
 
 mongo = PyMongo(app)
 
 cors = CORS(app, resources={r"/api/*": {"origins": "*"}})
 
+last_update_on_benchmark = datetime.now()
+
+def get_spy_benchmark():
+    url = 'https://www.alphavantage.co/query?function=TIME_SERIES_DAILY_ADJUSTED&symbol=SPY&apikey=Y0Y9D9IXUGG5PJ11'
+    r = requests.get(url)
+    data = r.json()
+    last_update_on_benchmark = datetime.now()
+    print("Benchmark updated and timestamp set to:", last_update_on_benchmark)
+    return data['Time Series (Daily)']
+
+def update_benchmark():
+    now = datetime.now()
+    if (now - last_update_on_benchmark).days > 0:
+        get_spy_benchmark()
+
+benchmarks = get_spy_benchmark()
+
+def normalize(array):
+    base = float(array[0]['y'])
+    for a in array:
+        a['y'] = int( float(a['y'])/ base * 100 )
+
 @app.route('/api/port/<string:id>', methods=['GET'])
 def get_port_by_id(id):
     
+    update_benchmark()
+    print(benchmarks)
+
     port_coll = mongo.db.stock_portfolios
     port = port_coll.find_one({'portId' : id})    
     port['_id'] = str(port['_id'])
     for p in port['portfolio']:
         p['_id'] = str(p['_id'])
+
+    # adding benchmark data
+    series = port['performance']
+    bm = []
+    for d in series:
+        print(d)
+        time_string = datetime.fromtimestamp(int(d['x']/1000) - 86400).strftime('%Y-%m-%d')
+        bm.append({
+                'x': d['x'],
+                'y': benchmarks[time_string]["5. adjusted close"]
+            })
+
+    normalize(bm)
+    normalize(port['performance'])
+    port['benchmark'] = bm
 
     response = jsonify(port)
     response.headers.add("Access-Control-Allow-Origin", "*")
